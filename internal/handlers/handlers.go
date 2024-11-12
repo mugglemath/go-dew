93c27fe3adad15e6ac0bf0b75b8a3b5b7220a7ac
noble <20589051+mugglemath@users.noreply.github.com>
2024-11-12 01:20:00 -0800

    changed 'windows' key to 'windowsShouldBe' with values of 'open' or 'closed'

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -41,7 +41,7 @@ func HandleSensorData(c *gin.Context) {
 	indoorDewpoint := data["indoorDewpoint"].(float64)
 	outdoorDewpoint := data["outdoorDewpoint"].(float64)
 	dewpointDelta := data["dewpointDelta"].(float64)
-	openWindows := data["openWindows"].(bool)
+	windowsShouldBe := data["windowsShouldBe"].(string)
 	humidityAlert := data["humidityAlert"].(bool)
 	isoTimestamp := time.Now().Format(time.RFC3339)
 
@@ -51,10 +51,10 @@ func HandleSensorData(c *gin.Context) {
 		"Indoor Dewpoint = %.2f C\n"+
 		"Outdoor Dewpoint = %.2f C\n"+
 		"Dewpoint Delta = %.2f C\n"+
-		"Open Windows = %t\n"+
+		"Windows Should Be = %s\n"+
 		"Humidity Alert = %t",
 		isoTimestamp, indoorTemperature, indoorHumidity, indoorDewpoint,
-		outdoorDewpoint, dewpointDelta, openWindows, humidityAlert)
+		outdoorDewpoint, dewpointDelta, windowsShouldBe, humidityAlert)
 
 	discord.SendDiscordMessage(message, os.Getenv("DISCORD_SENSOR_FEED_WEBHOOK_URL"))
 	fmt.Printf("Received data from C++ app: %v\n", data)
@@ -72,15 +72,15 @@ func HandleWindowAlert(c *gin.Context) {
 	indoorDewpoint := data["indoorDewpoint"].(float64)
 	outdoorDewpoint := data["outdoorDewpoint"].(float64)
 	dewpointDelta := data["dewpointDelta"].(float64)
-	openWindows := data["openWindows"].(bool)
+	windowsShouldBe := data["windowsShouldBe"].(string)
 	isoTimestamp := time.Now().Format(time.RFC3339)
 
 	message := fmt.Sprintf("%s\n@everyone\n"+
 		"Indoor Dewpoint = %.2f C\n"+
-		"Outdoor Dewpoint = %.2f %%\n"+
+		"Outdoor Dewpoint = %.2f C\n"+
 		"Dewpoint Delta = %.2f C\n"+
-		"Open Windows = %t\n",
-		isoTimestamp, indoorDewpoint, outdoorDewpoint, dewpointDelta, openWindows)
+		"Windows Should Be = %s\n",
+		isoTimestamp, indoorDewpoint, outdoorDewpoint, dewpointDelta, windowsShouldBe)
 
 	discord.SendDiscordMessage(message, os.Getenv("DISCORD_WINDOW_ALERT_WEBHOOK_URL"))
 	fmt.Printf("Received data from C++ app: %v\n", data)
